dd3924fd30bd84e6d5ef69b1d2a4bd919aedba4c
Sasha <hagrida@yandex.ru>
2017-04-08 22:00:18 +0300

    Модули
    Объявление модулей в проекте

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,9 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application',        
+        'Application', 
+        'App',
+        'AppAdmin',       
     ),
 
     // These are various options for the listeners attached to the ModuleManager
