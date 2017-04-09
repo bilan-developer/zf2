47be58eba1c19b7e9c09e4796debed74d536d37b
Sasha <hagrida@yandex.ru>
2017-04-09 20:40:24 +0300

    Doctrine
    Добавлен модуль Doctrine, осуществлена генерация сущностей и вывод одной
    из сущности в view, установлен модуль Migration

--- module/App/config/module.config.php
+++ module/App/config/module.config.php
@@ -8,6 +8,27 @@
  */
 
 return array(
+    'doctrine' => array(
+        'driver' => array(
+            // defines an annotation driver with two paths, and names it `my_annotation_driver`
+            'app_entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__.'/../src/App/Entity',
+                ),
+            ),
+
+            // default metadata driver, aggregates all other drivers into a single one.
+            // Override `orm_default` only if you know what you're doing
+            'orm_default' => array(
+                'drivers' => array(
+                    // register `my_annotation_driver` for any entity under namespace `My\Namespace`
+                    'App\Entity' => 'app_entity'
+                )
+            )
+        )
+    ),
     'router' => array(
         'routes' => array(
            'app' => array(
